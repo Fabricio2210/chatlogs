617081dcd459382976e18f8b02fe996b7e2612a7
fabricio2210 <fabricio22101985@gmail.com>
2023-11-05 20:27:40 -0300

    test: testing pipeline

--- main.go
+++ main.go
@@ -2,8 +2,56 @@ package main
 
 import (
 	"fmt"
+	"github.com/Fabricio2210/wrapper"
+	"github.com/robfig/cron"
 )
 
 func main() {
-	fmt.Println("teste")
+	c := cron.New()
+	c.AddFunc("0 10 11 * *", func() {
+		wrapper.Wrapper("DSP")
+	})
+	c.AddFunc("0 15 11 * *", func() {
+		wrapper.Wrapper("DDM")
+	})
+	c.AddFunc("0 20 11 * *", func() {
+		wrapper.Wrapper("RAW")
+	})
+	c.AddFunc("0 25 11 * *", func() {
+		wrapper.Wrapper("POP")
+	})
+	c.AddFunc("0 30 11 * *", func() {
+		wrapper.Wrapper("SHINKO")
+	})
+	c.AddFunc("0 35 11 * *", func() {
+		wrapper.Wrapper("AQUA")
+	})
+	c.AddFunc("0 40 11 * *", func() {
+		wrapper.Wrapper("BEAM")
+	})
+	c.AddFunc("0 45 11 * *", func() {
+		wrapper.Wrapper("DECEPTICRON")
+	})
+	c.AddFunc("0 50 11 * *", func() {
+	 	wrapper.Wrapper("DOODY")
+	})
+	c.AddFunc("0 55 11 * *", func() {
+		wrapper.Wrapper("MEERKAT")
+	})
+	c.AddFunc("0 00 12 * *", func() {
+		wrapper.Wrapper("PROPER")
+	})
+	c.AddFunc("0 05 12 * *", func() {
+		wrapper.Wrapper("REACTS")
+	})
+	c.AddFunc("0 10 12 * *", func() {
+		wrapper.Wrapper("TBS")
+	})
+	c.AddFunc("0 15 12 * *", func() {
+		wrapper.Wrapper("WPIG")
+	})
+	c.Start()
+	fmt.Println("Running")
+	<-make(chan struct{})
 }
+
